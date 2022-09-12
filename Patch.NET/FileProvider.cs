85a4f51106f5da85d1fe2032f186757263c77a3d
sardelka9515 <sardelka9515@gmail.com>
2022-09-13 07:06:57 +0800

    Add project files.

---
+++ Patch.NET/FileProvider.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatchDotNet
+{
+    public class FileFragement
+    {
+        public long StartPosition;
+        public long EndPosition;
+        public long ReadPosition;
+        public BinaryReader Reader;
+    }
+    public class FragmentComparer : IComparer<FileFragement>
+    {
+        public int Compare(FileFragement x, FileFragement y)
+        {
+            if (x.StartPosition < y.StartPosition)
+            {
+                return -1;
+            }
+            else if (x.StartPosition > y.StartPosition)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+    public class FileProvider
+    {
+        FileStream BaseStream;
+        Patch Current;
+        List<FileFragement> Fragements;
+        static FragmentComparer Comparer = new FragmentComparer();
+        public void Test() {
+        }
+        public FileProvider(string baseFile, bool canWrite, params string[] snapshots)
+        {
+            BaseStream = new FileStream(baseFile, FileMode.Open, FileAccess.Read);
+            Fragements = new List<FileFragement>() {
+                new FileFragement {
+                    StartPosition=0,
+                    EndPosition=BaseStream.Length,
+                    ReadPosition=0,
+                    Reader=new BinaryReader(BaseStream)
+                }
+            };
+            for (int i = 0; i < snapshots.Length - 1; i++)
+            {
+                Console.WriteLine("Reading records from "+ snapshots[i]);
+                var snapshot = new Patch(snapshots[i], false);
+                int read = 0;
+                while (snapshot.ReadRecord(out var type, out var vPosOrSize, out var readPos, out var chunkLen))
+                {
+                    if (type == RecordType.Write)
+                    {
+                        MapRecord(vPosOrSize,readPos,chunkLen,snapshot.Reader);
+                    }
+                    else if (type == RecordType.SetLength)
+                    {
+
+                    }
+                    read++;
+                    Console.Write("\rRead " +read+" records");
+                }
+            }
+            Current = new Patch(snapshots[snapshots.Length - 1], canWrite);
+        }
+        void MapRecord(long vPos, long readPos, int chunkLen, BinaryReader reader)
+        {
+            var newFrag = new FileFragement
+            {
+                StartPosition = vPos,
+                EndPosition = vPos + chunkLen,
+                ReadPosition = readPos,
+                Reader = reader
+            };
+            var index = Fragements.BinarySearch(newFrag, Comparer);
+
+            // No frag with same starting position
+            if (index < 0)
+            {
+                index = ~index;
+                Fragements.Insert(index, newFrag);
+                RemoveOverlapped(index);
+            }
+            else
+            {
+                Fragements[index] = newFrag;
+                RemoveOverlapped(index);
+            }
+        }
+        void RemoveOverlapped(int newFragIndex)
+        {
+            var newFrag=Fragements[newFragIndex];
+
+            // Remove overlapped parts
+            int remove = 0;
+            for (int i = newFragIndex + 1; i < Fragements.Count; i++)
+            {
+                var tocheck = Fragements[i];
+                if (tocheck.EndPosition <= newFrag.EndPosition)
+                {
+                    remove++;
+                }
+                else if (tocheck.StartPosition <= newFrag.EndPosition)
+                {
+                    var offset = tocheck.StartPosition - 1 - newFrag.EndPosition;
+                    tocheck.StartPosition += offset;
+                    tocheck.ReadPosition += offset;
+                    break;
+                }
+                else if (tocheck.StartPosition == newFrag.EndPosition + 1)
+                {
+                    break;
+                }
+                else
+                {
+                    throw new NotImplementedException();
+                }
+
+            }
+            if (remove > 0)
+            {
+                Fragements.RemoveRange(newFragIndex + 1, remove);
+                Console.WriteLine($"{remove} fragements removed");
+            }
+        }
+        public void Write(long position,byte[] chunk)
+        {
+            MapRecord(position, Current.Write(position, chunk), chunk.Length, Current.Reader);
+        }
+    }
+}
---
+++ Patch.NET/Patch.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PatchDotNet
+{
+    public enum RecordType
+    {
+        None,
+        Write,
+        SetLength
+    }
+    public class Record
+    {
+        public RecordType Type;
+        public long Position;
+        public int Length;
+        public long ReadPosition;
+    }
+    public class Patch
+    {
+        public bool CanWrite => Write != null;
+        private readonly FileStream _stream;
+        public readonly BinaryReader Reader;
+        public readonly BinaryWriter Writer;
+        private long _lastVirtualPosition = -1;
+        private int _lastChunkSize;
+        public Patch(string path, bool canWrite)
+        {
+            _stream = new FileStream(path, FileMode.OpenOrCreate, canWrite ? FileAccess.ReadWrite : FileAccess.Read, FileShare.None);
+            Reader = new BinaryReader(_stream);
+            Writer = canWrite ? new BinaryWriter(_stream) : null;
+        }
+
+        public long Write(long virtualPosition, byte[] chunk)
+        {
+            // if (Writer == null) { throw new InvalidOperationException("Snapshot does not support writing"); }
+
+            // sequential write defragmention
+            if (virtualPosition > 0 && virtualPosition == _lastVirtualPosition + _lastChunkSize)
+            {
+                // Seek to last record for modifying chunk length
+                Writer.Seek(-(_lastChunkSize + sizeof(int)), SeekOrigin.End);
+
+                // TODO: add intergrity check here
+
+                // Modify chunk size
+                Writer.Write(_lastChunkSize = _lastChunkSize + chunk.Length);
+
+                // Finally, write chunk to the end
+                Writer.Seek(0, SeekOrigin.End);
+                Writer.Write(chunk, 0, chunk.Length);
+            }
+            else
+            {
+
+                Writer.Seek(0, SeekOrigin.End);
+                Writer.Write(virtualPosition);
+                Writer.Write(chunk.Length);
+                Writer.Write(chunk, 0, chunk.Length);
+                _lastVirtualPosition = virtualPosition;
+                _lastChunkSize = chunk.Length;
+
+            }
+            return _stream.Position - chunk.Length;
+        }
+        public void Resize(long newSize)
+        {
+            Writer.Seek(0, SeekOrigin.End);
+            Writer.Write(newSize);
+
+            // indicates that this is a resize record
+            Writer.Write(0);
+        }
+
+        /// <summary>
+        /// (Write, position, readPosition, length), (SetLength, newlength, 0, 0)
+        /// 
+        /// Write record schema:
+        /// {long:virtualPosition}{int:chunkLength}{byte[legnth]:chunk}
+        /// 
+        /// SetLength record schema:
+        /// {long:newLength}{int=0}
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public bool ReadRecord(out RecordType type, out long virtualPositionOrNewSize, out long readPosition, out int chunkLength)
+        {
+            /*
+            if (!_stream.CanRead)
+            {
+                type = RecordType.None;
+                virtualPositionOrNewSize = 0;
+                readPosition = 0;
+                chunkLength = 0;
+                return false;
+            }
+            */
+
+            // virtual file position or new length
+            virtualPositionOrNewSize = Reader.ReadInt64();
+
+            // chunk length
+            chunkLength = Reader.ReadInt32();
+
+            // SetLength record
+            if (chunkLength == 0)
+            {
+                type = RecordType.SetLength;
+                readPosition = 0;
+            }
+            else
+            {
+                type = RecordType.Write;
+                readPosition = _stream.Position;
+
+                // Seek position for reading next record
+                _stream.Seek(chunkLength, SeekOrigin.Current);
+            }
+
+
+            return _stream.CanRead;
+        }
+    }
+}
