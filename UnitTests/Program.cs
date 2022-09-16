e6b5e15bb4cc41e87787b6284e84b8b0d09f1161
sardelka9515 <sardelka9515@gmail.com>
2022-09-15 21:39:13 -0700

    blah

--- UnitTests/Program.cs
+++ UnitTests/Program.cs
@@ -1,13 +1,29 @@
 ﻿using PatchDotNet;
-
+using PatchDotNet.Win32;
+using DokanNet;
+using DokanNet.Logging;
 namespace UnitTests
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            RecordTest.Run();
-            FileTest.Run();
+            
+            // var mount=new SingleFileMount(new FileProvider(@"C:\test.vhdx",true,@"C:\Patch1"),"base.vhdx", new ConsoleLogger("[Mirror] "));
+            var mount=new Mirror(new ConsoleLogger("[Mirror] "),@"C:\");
+            using var dokan = new Dokan(new ConsoleLogger("[Dokan] "));
+            var safeDokanBuilder = new DokanInstanceBuilder(dokan)
+                .ConfigureOptions(options =>
+                {
+                    options.Options = DokanOptions.MountManager|DokanOptions.DebugMode;
+                    options.MountPoint = @"C:\mount";
+                }).ConfigureLogger(()=>new ConsoleLogger("[Dokan Logger] "));
+            var dokanBuilder = new DokanInstanceBuilder(dokan);
+            var dokanInstance = dokanBuilder.Build(mount);
+            Console.ReadLine();
+
+            // RecordTest.Run();
+            // FileTest.Run();
         }
     }
 }
